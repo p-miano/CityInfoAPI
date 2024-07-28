4ab9731ea5e1c401ed91914c2433850b36822acf
p-miano <103672587+p-miano@users.noreply.github.com>
2024-07-28 08:42:08 -0300

    Added functionalities for creating, updating and deleting points of interest. Also added functionality for uploading files.

--- CityInfo.API/Controllers/FilesController.cs
+++ CityInfo.API/Controllers/FilesController.cs
@@ -41,5 +41,28 @@ namespace CityInfo.API.Controllers
             var bytes = System.IO.File.ReadAllBytes(pathToFile);
             return File(bytes, contentType, Path.GetFileName(pathToFile));
         }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateFile(IFormFile file)
+        {
+            // Validate the input. Put a limit on filesize to avoid large uploads attacks. 
+            // Only accept .pdf files (check content-type)
+            if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+            {
+                return BadRequest("No file or an invalid one has been inputted.");
+            }
+
+            // Create the file path.  Avoid using file.FileName, as an attacker can provide a malicious one, including full paths or relative paths.  Also, in production enviroments, the file should be uploaded to a different location with no execution permissions to avoid security risks.
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("Your file has been uploaded successfully.");
+        }
     }
 }
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
-}).AddXmlDataContractSerializerFormatters(); // Add support for XML
+}).AddNewtonsoftJson() // Add support for JsonPatchDocument
+    .AddXmlDataContractSerializerFormatters(); // Add support for XML
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
